7a73b89c695c79a1346dd807bd43ee51a375d162
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-09-12 12:29:30 -0300

    Criando um arquivo/componente para estilização global da aplicação

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import React from "react";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 
+import { GlobalStyle } from "./Components/GlobalStyle";
+
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Cabecalho />
       <Container />
     </>
