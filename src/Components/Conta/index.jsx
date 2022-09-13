f23a2b043cef4c0cecb07a3f8492ed3a3b93d6af
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-09-13 17:06:43 -0300

    Fazendo a herança e sobrecarda de um styled component

--- src/Components/Conta/index.jsx
+++ src/Components/Conta/index.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import styled from 'styled-components'
 import { Icone } from '../../Components/UI/index'
 
 import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
 import dinheiro from "../../assets/images/dinheiro.svg";
+
+const IconeMargin = styled(Icone)`
+  margin-top: 2px;
+`
 
 const Conta = () => {
   const [toggleState, untoggle] = useState(true);
@@ -28,8 +33,7 @@ const Conta = () => {
       </div>
 
       <button className="btn" onClick={toggleHandler}>
-        <Icone
-          style={{ marginTop: "2px" }}
+        <IconeMargin
           src={toggleState ? privado : olho_icone}
           alt="Privacidade do Saldo"
         />
