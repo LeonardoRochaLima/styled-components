701be9edf063230fd52774ea2135bcfe8900ee25
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-09-13 16:57:19 -0300

    Criando arquivo de abstração de styled components

--- src/Components/Conta/index.jsx
+++ src/Components/Conta/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Icone } from '../../Components/UI/index'
 
 import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
@@ -17,7 +18,7 @@ const Conta = () => {
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponível{" "}
         <span>
-          <img className="imagem-icone" src={dinheiro} alt="Ícone Saldo" />
+          <Icone src={dinheiro} alt="Ícone Saldo" />
         </span>
         {toggleState ? (
           <div className="saldo">
@@ -27,9 +28,8 @@ const Conta = () => {
       </div>
 
       <button className="btn" onClick={toggleHandler}>
-        <img
+        <Icone
           style={{ marginTop: "2px" }}
-          className="imagem-icone"
           src={toggleState ? privado : olho_icone}
           alt="Privacidade do Saldo"
         />
