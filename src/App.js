e2baa3c9ab727ad22469e3ca6ef455d6bda740c0
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-09-15 14:20:14 -0300

    Chamando as funções na raiz da aplicação para fazer switch do tema

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { BtnTema } from "./Components/UI";
+import SwitcherTema from "./Components/SwitcherTema";
+import { useState } from "react";
 
 function App() {
+  const [tema, setTema] = useState(true)
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  }
   return (
-    <ThemeProvider theme={temaEscuro}>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <BtnTema onClick={() => {toggleTema()}}>
+        <SwitcherTema tema={tema}/>
+      </BtnTema>
       <Cabecalho />
       <Container />
     </ThemeProvider>
